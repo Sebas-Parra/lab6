52378168ebf282b1758e4ea10c45c623dbe59960
Sebastian Parra <sebasraster10@gmail.com>
2026-01-27 11:54:20 -0500

    firs commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { log } = require('node:console');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+    res.send('Integración continua funcionando');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor esta ejecutandose en el puerto ${PORT}`);
+})
